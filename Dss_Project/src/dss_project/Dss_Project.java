72b2987daa898be0498e4a0807b2b1a38858244d
Marina Miguélez <m.miguelez2@usp.ceu.es>
2022-03-10 17:59:40 +0100

    Disease main
    Co-Authored-By: pelopintxo <47748431+pelopintxo@users.noreply.github.com>
    Co-Authored-By: apicella07 <61517968+apicella07@users.noreply.github.com>

--- Dss_Project/src/dss_project/Dss_Project.java
+++ Dss_Project/src/dss_project/Dss_Project.java
@@ -11,12 +11,29 @@ package dss_project;
  */
 public class Dss_Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         Disease IBS= new Disease("Irritable Bowel Syndrom",Disease.Gender.FEMALE,Disease.AgeRange.YOUNG,true,true,true,true,true,false,false,true,true,false,false,true,true,true,false,false,false,false,false,false,false,false,false);
+        
+        Disease Chron = new Disease("Chron",Disease.Gender.FEMALE,Disease.AgeRange.YOUNGADULT,true,true,true,true,true,true,true,true,false,true,true,false,true,false,false,true,false,false,true,false,false,false,false);
+        
+        Disease Ulcerative_colitis= new Disease("Ulcerative Colitis",Disease.Gender.MALE,Disease.AgeRange.YOUNGADULT,true,true,false,false,true,true,false,true,true,true,true,false,true,false,false,false,false,false,false,false,true,true,false);
+        
+        Disease Diverticulosis= new Disease("Diverticulosis",Disease.Gender.FEMALE,Disease.AgeRange.ADULT,true,false,true,false,false,false,true,true,true,false,false,false,false,true,false,true,false,false,false,true,true,false,false);
+        
+        Disease Hernia= new Disease("Inguinal or Abdominal Hernia",Disease.Gender.MALE,Disease.AgeRange.CHILD,true,false,true,true,false,false,false,false,true,false,false,false,true,false,true,true,false,false,false,false,false,false,false);
+        
+        Disease Appendicitis= new Disease("Appendicitis",Disease.Gender.MALE,Disease.AgeRange.YOUNGADULT,true,true,true,true,false,false,true,true,false,false,false,true,false,false,false,true,false,false,false,false,false,false,false);
+        
+        Disease Enterocolitis= new Disease("Infectious Enterocolitis",null,Disease.AgeRange.CHILD,true,false,true,true,false,false,false,true,true,true,false,false,false,false,false,true,true,true,false,false,false,false,false);
+        
+        Disease Celiac_Disease = new Disease("Celiac Disease",Disease.Gender.FEMALE,Disease.AgeRange.ADULT,true,false,true,true,true,true,false,true,false,true,false,false,true,false,false,false,false,false,false,false,false,false,true);
+        
+        Disease Colorectal_cancer= new Disease("Colorectal Cancer",Disease.Gender.MALE,Disease.AgeRange.ADULT,true,true,true,true,true,true,true,true,true,true,true,false,true,false,false,false,false,false,false,false,false,false,false);
+        
+        
+    
+    
     }
     
 }
