ace935a74d013b4db995d59392448f9723a45dda
apicella07 <gabriela.apicella@hotmail.com>
2022-03-23 17:18:21 +0100

    modified java classes from gui
    gui class modification questions 3 and 4

--- DSS_Abdominal/src/gui/Questions3.java
+++ DSS_Abdominal/src/gui/Questions3.java
@@ -5,6 +5,7 @@
  */
 package gui;
 
+import dss_abdominal.Patient;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
@@ -15,6 +16,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
@@ -24,100 +26,186 @@ import javafx.stage.Stage;
  */
 public class Questions3 {
     
+      @FXML
+    private RadioButton fever_low;
+
     @FXML
-    private  RadioButton fever_none;
-    
-     @FXML
     private  RadioButton fever_medium;
-    
-          @FXML
+
+    @FXML
     private  RadioButton fever_high;
-    
-     @FXML
-    private  RadioButton discomfortanus_none;
-     
-      @FXML
-    private  RadioButton discomfortanus_focalized;
-    
-           @FXML
-    private  RadioButton discomfortanus_extended;
-    
-     
-      @FXML
+
+    @FXML
+    private  RadioButton discomfort_none;
+
+    @FXML
+    private  RadioButton discomfort_focalized;
+
+    @FXML
+    private  RadioButton discomfort_extended;
+
+    @FXML
     private  RadioButton cramps_none;
-      
-       @FXML
+
+    @FXML
     private  RadioButton cramps_mild;
     
-            @FXML
+     @FXML
     private  RadioButton cramps_disabling;
-   
-        @FXML
+     
+      @FXML
     private  RadioButton melenas_none;
       
-           @FXML
-    private  RadioButton melenas_some;
-      
-        
        @FXML
-    private  RadioButton melenas_alot;
+    private  RadioButton melenas_few;
        
-          @FXML
+        @FXML
+    private  RadioButton melenas_substantially;
+        
+        @FXML
     private  RadioButton dermatitis_none;
-       
-              @FXML
-    private  RadioButton dermatitis_focalized;
-       
-                  @FXML
+        
+        @FXML
     private  RadioButton dermatitis_extended;
-       
+        
+        @FXML
+    private  RadioButton dermatitis_focalized;
 
-         
-         private ToggleGroup answers;
-         
-    
-        public void initialize (URL url, ResourceBundle rb){
-            
-            answers = new ToggleGroup();
-            this.fever_none.setToggleGroup(answers);
-            this.fever_medium.setToggleGroup(answers);
-            this.fever_high.setToggleGroup(answers);
-            this.discomfortanus_none.setToggleGroup(answers);
-            this.discomfortanus_focalized.setToggleGroup(answers);
-             this.discomfortanus_extended.setToggleGroup(answers);
-            this.cramps_none.setToggleGroup(answers);
-            this.cramps_mild.setToggleGroup(answers);
-            this.cramps_disabling.setToggleGroup(answers);
-            this.melenas_none.setToggleGroup(answers);
-            this.melenas_some.setToggleGroup(answers);
-            this.melenas_alot.setToggleGroup(answers);
-            this.dermatitis_none.setToggleGroup(answers);
-            this.dermatitis_focalized.setToggleGroup(answers);
-            this.dermatitis_extended.setToggleGroup(answers);
-            
+
+    private ToggleGroup fever;
+    private ToggleGroup discomfort;
+    private ToggleGroup cramps;
+    private ToggleGroup melenas;
+    private ToggleGroup dermatitis;
+
+    private MainWindow main;
+    private Patient patient;
+
+
+    public void initialize (URL url, ResourceBundle rb){
+
+        fever = new ToggleGroup();
+        discomfort = new ToggleGroup();
+        cramps = new ToggleGroup();
+        melenas = new ToggleGroup();
+        dermatitis  = new ToggleGroup();
+
+
+        this.fever_low.setToggleGroup(fever);
+        this.fever_medium.setToggleGroup(fever);
+        this.fever_high.setToggleGroup(fever);
+        this.discomfort_none.setToggleGroup(discomfort);
+        this.discomfort_focalized.setToggleGroup(discomfort);
+        this.discomfort_extended.setToggleGroup(discomfort);
+        this.cramps_none.setToggleGroup(cramps);
+        this.cramps_mild.setToggleGroup(cramps);
+        this.cramps_disabling.setToggleGroup(cramps);
+        this.melenas_none.setToggleGroup(melenas);
+        this.melenas_few.setToggleGroup(melenas);
+        this.melenas_substantially.setToggleGroup(melenas);
+        this.dermatitis_none.setToggleGroup(dermatitis);
+        this.dermatitis_focalized.setToggleGroup(dermatitis);
+        this.dermatitis_extended.setToggleGroup(dermatitis);
+    }
+
+
+    public void initComponents(MainWindow main, Patient patient) {
+        this.main = main;
+        this.patient = patient;
+
+        if ( (int) (patient.getFever())==0 ) {
+            fever.selectToggle(fever_low);
+        } else if ((int) (patient.getFever())==1){
+            fever.selectToggle(fever_medium);
+        }
+        else if ((int) (patient.getFever())==2){
+            fever.selectToggle(fever_high);
         }
 
-   public void gotoquestions4( ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
+        if ( (int) (patient.getPerianalDiscom()) == 0 ){
+            discomfort.selectToggle(discomfort_none);
+        } else if ( (int) (patient.getPerianalDiscom()) == 1 ){
+            discomfort.selectToggle(discomfort_focalized);
+        } else if ( (int) (patient.getPerianalDiscom()) == 2 ){
+            discomfort.selectToggle(discomfort_extended);
+        }
 
-        loader.setLocation(getClass().getResource("Questions4.fxml"));
-        Parent parent = loader.load();
+        if ( (int) (patient.getCramps()) == 0 ){
+            cramps.selectToggle(cramps_none);
+        } else if ( (int) (patient.getCramps()) == 1 ){
+            cramps.selectToggle(cramps_mild);
+        } else if ( (int) (patient.getCramps()) == 2 ){
+            cramps.selectToggle(cramps_disabling);
+        }
+/*
+        if ( (int) patient.ge()== 0) {
+            melenas.selectToggle(melenas_none);
+        } else if ( (int) patient.getNausea() == 1) {
+            nausea_toggle.selectToggle(nausea_sporadic);
+        } else if ( (int) patient.getNausea() == 2) {
+            nausea_toggle.selectToggle(nausea_frequent);
+        }
 
-        Scene scene = new Scene(parent);
-    
-  }
-    
-   
-  public void gobackquestions2( ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
+        if ( (int) (patient.()) == 0 ){
+            discomfort.selectToggle(discomfort_none);
+        } else if ( (int) (patient.getPerianalDiscom()) == 1 ){
+            discomfort.selectToggle(discomfort_focalized);
+        } else if ( (int) (patient.getPerianalDiscom()) == 2 ){
+            discomfort.selectToggle(discomfort_extended);
+        }
+        if ( patient.isAnxiety() ) {
+            anxiety_toggle.selectToggle(anxiety_depress_yes);
+        } else {
+            anxiety_toggle.selectToggle(anxiety_depress_no);
+        }
+*/
+    }
+/*
+    @Override
+    public void getQuestionValues() {
+        Toggle fever_selected = fever.getSelectedToggle();
+        Toggle discomfort_selected = discomfort.getSelectedToggle();
+        Toggle cramps_selected = cramps.getSelectedToggle();
+        Toggle melenas_selected = melenas.getSelectedToggle();
+        Toggle dermatitis_selected = dermatitis.getSelectedToggle();
 
-        loader.setLocation(getClass().getResource("Questions2.fxml"));
-        Parent parent = loader.load();
+        patient.setFever(fever_selected == fever_medium);
+        patient.setAnxiety( anxiety_selected == anxiety_depress_yes);
+        patient.setDiarrhea( diarrhea_selected == diarrhea_yes );
 
-        Scene scene = new Scene(parent);
-    
-  }
-        
+        if ( nausea_selected == nausea_none ){
+            patient.setNausea(0);
+        } else if ( nausea_selected == nausea_sporadic ) {
+            patient.setNausea(1);
+        } else if ( nausea_selected == nausea_frequent ) {
+            patient.setNausea(2);
+        }
+
+        if ( swelling_selected == abdominalSwelling_none ){
+            patient.setDistention(0);
+        } else if ( nausea_selected == abdominalSwelling_low ) {
+            patient.setDistention(1);
+        } else if ( nausea_selected == abdominalSwelling_high ) {
+            patient.setDistention(2);
+        }
+
+    }
+*/
+    @FXML
+    public void checkRadialButtons(javafx.event.ActionEvent evnt){
+        Toggle fever_selected = fever.getSelectedToggle();
+        Toggle discomfort_selected = discomfort.getSelectedToggle();
+        Toggle cramps_selected = cramps.getSelectedToggle();
+        Toggle melenas_selected = melenas.getSelectedToggle();
+        Toggle dermatitis_selected = dermatitis.getSelectedToggle();
+
+        if ( fever_selected == null || discomfort_selected == null || cramps_selected == null || melenas_selected == null || dermatitis_selected ==null ) {
+            main.button_next.setDisable(true);
+        } else {
+            main.button_next.setDisable(false);
+        }
+
+    }
+
 
 }
-        
--- DSS_Abdominal/src/gui/Questions4.java
+++ DSS_Abdominal/src/gui/Questions4.java
@@ -6,6 +6,7 @@
 package gui;
 
 
+import dss_abdominal.Patient;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
@@ -16,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
@@ -24,90 +26,162 @@ import javafx.stage.Stage;
  * @author marin
  */
 public class Questions4 {
-    
+     
+      @FXML
+    private RadioButton chill_none;
+
     @FXML
-    private  RadioButton chills_none;
-    
-     @FXML
-    private  RadioButton chills_sporadic;
-    
-          @FXML
-    private  RadioButton chills_frequent;
-    
-     @FXML
-    private  RadioButton increasedpoo_none;
-     
-      @FXML
-    private  RadioButton increasedpoo_mild;
-    
-           @FXML
-    private  RadioButton increasedpoo_disabling;
-    
-     
-      @FXML
+    private  RadioButton chill_sporadic;
+
+    @FXML
+    private  RadioButton chill_frequent;
+
+    @FXML
+    private  RadioButton poo_none;
+
+    @FXML
+    private  RadioButton poo_mild;
+
+    @FXML
+    private  RadioButton poo_disabling;
+
+    @FXML
     private  RadioButton fatigue_none;
-      
-       @FXML
+
+    @FXML
     private  RadioButton fatigue_sporadic;
     
-            @FXML
+     @FXML
     private  RadioButton fatigue_frequent;
-   
-        @FXML
+     
+      @FXML
     private  RadioButton vomiting_none;
       
-           @FXML
-    private  RadioButton vomiting_sporadic;
-      
-        
        @FXML
-    private  RadioButton vomiting_frequent;
+    private  RadioButton vomiting_sporadic;
        
-      
+        @FXML
+    private  RadioButton vomiting_frequent;
+        
 
-         
-         private ToggleGroup answers;
-         
-    
-        public void initialize (URL url, ResourceBundle rb){
-            
-            answers = new ToggleGroup();
-            this.chills_none.setToggleGroup(answers);
-            this.chills_sporadic.setToggleGroup(answers);
-            this.chills_frequent.setToggleGroup(answers);
-            this.increasedpoo_none.setToggleGroup(answers);
-            this.increasedpoo_mild.setToggleGroup(answers);
-             this.increasedpoo_disabling.setToggleGroup(answers);
-            this.fatigue_none.setToggleGroup(answers);
-            this.fatigue_sporadic.setToggleGroup(answers);
-            this.fatigue_frequent.setToggleGroup(answers);
-            this.vomiting_none.setToggleGroup(answers);
-            this.vomiting_sporadic.setToggleGroup(answers);
-            this.vomiting_frequent.setToggleGroup(answers);
-            
+
+
+    private ToggleGroup chills;
+    private ToggleGroup poo;
+    private ToggleGroup fatigue;
+    private ToggleGroup vomiting;
+
+
+    private MainWindow main;
+    private Patient patient;
+
+
+    public void initialize (URL url, ResourceBundle rb){
+
+        chills = new ToggleGroup();
+        poo = new ToggleGroup();
+        fatigue = new ToggleGroup();
+        vomiting = new ToggleGroup();
+
+
+        this.chill_none.setToggleGroup(chills);
+        this.chill_sporadic.setToggleGroup(chills);
+        this.chill_frequent.setToggleGroup(chills);
+        this.poo_none.setToggleGroup(poo);
+        this.poo_mild.setToggleGroup(poo);
+        this.poo_disabling.setToggleGroup(poo);
+        this.fatigue_none.setToggleGroup(fatigue);
+        this.fatigue_sporadic.setToggleGroup(fatigue);
+        this.fatigue_frequent.setToggleGroup(fatigue);
+        this.vomiting_none.setToggleGroup(vomiting);
+        this.vomiting_sporadic.setToggleGroup(vomiting);
+        this.vomiting_frequent.setToggleGroup(vomiting);
+
+    }
+
+
+    public void initComponents(MainWindow main, Patient patient) {
+        this.main = main;
+        this.patient = patient;
+/*CHILLS & POO
+        if ( (int) (patient.get())==0 ) {
+            fever.selectToggle(fever_low);
+        } else if ((int) (patient.getFever())==1){
+            fever.selectToggle(fever_medium);
+        }
+        else if ((int) (patient.getFever())==2){
+            fever.selectToggle(fever_high);
+        } 
+
+        if ( (int) (patient.()) == 0 ){
+            discomfort.selectToggle(discomfort_none);
+        } else if ( (int) (patient.getPerianalDiscom()) == 1 ){
+            discomfort.selectToggle(discomfort_focalized);
+        } else if ( (int) (patient.getPerianalDiscom()) == 2 ){
+            discomfort.selectToggle(discomfort_extended);
+        }
+*/
+        if ( (int) (patient.getFatigue()) == 0 ){
+            fatigue.selectToggle(fatigue_none);
+        } else if ( (int) (patient.getCramps()) == 1 ){
+            fatigue.selectToggle(fatigue_sporadic);
+        } else if ( (int) (patient.getCramps()) == 2 ){
+            fatigue.selectToggle(fatigue_frequent);
         }
 
-    public void gobackquestions3( ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
+        if ( (int) patient.getVomiting()== 0) {
+            vomiting.selectToggle(vomiting_none);
+        } else if ( (int) patient.getNausea() == 1) {
+            vomiting.selectToggle(vomiting_sporadic);
+        } else if ( (int) patient.getNausea() == 2) {
+            vomiting.selectToggle(vomiting_frequent);
+        }
 
-        loader.setLocation(getClass().getResource("Questions3.fxml"));
-        Parent parent = loader.load();
-
-        Scene scene = new Scene(parent);
-    
-  }
-    
-   
-   public void gotoResults( ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
-
-        loader.setLocation(getClass().getResource("Results.fxml"));
-        Parent parent = loader.load();
-
-        Scene scene = new Scene(parent);
-    
-  }
+    }
+/*
+    @Override
+    public void getQuestionValues() {
+        Toggle chills_selected = chills.getSelectedToggle();
+        Toggle poo_selected = poo.getSelectedToggle();
+        Toggle fatigue_selected = fatigue.getSelectedToggle();
+        Toggle vomiting_selected = vomiting.getSelectedToggle();
         
+        patient.setFever(fever_selected == fever_medium);
+        patient.setAnxiety( anxiety_selected == anxiety_depress_yes);
+        patient.setDiarrhea( diarrhea_selected == diarrhea_yes );
+
+        if ( nausea_selected == nausea_none ){
+            patient.setNausea(0);
+        } else if ( nausea_selected == nausea_sporadic ) {
+            patient.setNausea(1);
+        } else if ( nausea_selected == nausea_frequent ) {
+            patient.setNausea(2);
+        }
+
+        if ( swelling_selected == abdominalSwelling_none ){
+            patient.setDistention(0);
+        } else if ( nausea_selected == abdominalSwelling_low ) {
+            patient.setDistention(1);
+        } else if ( nausea_selected == abdominalSwelling_high ) {
+            patient.setDistention(2);
+        }
+
+    }
+*/
+    @FXML
+    public void checkRadialButtons(javafx.event.ActionEvent evnt){
+        Toggle chills_selected = chills.getSelectedToggle();
+        Toggle poo_selected = poo.getSelectedToggle();
+        Toggle fatigue_selected = fatigue.getSelectedToggle();
+        Toggle vomiting_selected = vomiting.getSelectedToggle();
+
+        if ( chills_selected == null || poo_selected == null || fatigue_selected == null || vomiting_selected == null ) {
+            main.button_next.setDisable(true);
+        } else {
+            main.button_next.setDisable(false);
+        }
+
+    }
+
 
 }
-        
