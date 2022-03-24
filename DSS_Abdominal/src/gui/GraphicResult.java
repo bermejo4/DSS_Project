d7546991ad6a472d4a69efbb7ef747b8a4f31ba5
Marina Miguélez <m.miguelez2@usp.ceu.es>
2022-03-24 23:00:27 +0100

    more graph

--- DSS_Abdominal/src/gui/GraphicResult.java
+++ DSS_Abdominal/src/gui/GraphicResult.java
@@ -5,19 +5,37 @@
  */
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
+import javafx.stage.Stage;
+import gui.MainWindow;
 
 /**
  *
  * @author marin
  */
-public class GraphicResult {
+public class GraphicResult implements Initializable {
+
+    private BarChart barChart;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Diseases");
+        series1.getData().add(new XYChart(x:"Chron's disease",y: MainWindow.));
+        
+        CategoryAxis xAxis=new CategoryAxis(); //We use this class of category because each disease is a category
+        //xAxis.setLabel("Diseases");
     
-    
-    CategoryAxis xAxis=new CategoryAxis(); //We use this class of category because each disease is a category
-    //xAxis.setLabel("Diseases");
-    
-    NumberAxis yAxis = new NumberAxis();
-    //yAxis.setLabel("Approximation percentage");
-     //BarChart 11 = new BarChart(xAxis,yAxis);
+        NumberAxis yAxis = new NumberAxis();
+        //yAxis.setLabel("Approximation percentage");
+        //BarChart 11 = new BarChart(xAxis,yAxis);
+
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
