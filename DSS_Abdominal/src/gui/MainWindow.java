ed4116997456c904aa695b06cc589130062512d3
bermejo4 <minijai72@gmail.com>
2022-03-24 18:08:25 +0100

    Update create model disease in mainWindow

--- DSS_Abdominal/src/gui/MainWindow.java
+++ DSS_Abdominal/src/gui/MainWindow.java
@@ -45,6 +45,17 @@ public class MainWindow implements Initializable {
     float celiacPorcentage=-1;
     float colorectalCancerPorcentage=-1;
     
+    Patient ibs = new Patient("Irritable Bowel Syndrom",Patient.Gender.FEMALE,Patient.AgeRange.YOUNG,1f,1f,1f,1f,true,false,false,true,1f,0f,0f,0f,true,1f,false,0f,0f,false,0f,0f,0f,0f,0f);    
+    Patient chron = new Patient("Chron",Patient.Gender.FEMALE,Patient.AgeRange.YOUNGADULT,1f,2f,1f,1f,true,true,true,true,0f,1f,2f,0f,true,0f,false,1f,0f,false,1f,0f,0f,0f,0f);        
+    Patient ulcerative_colitis = new Patient("Ulcerative Colitis",Patient.Gender.MALE,Patient.AgeRange.YOUNGADULT,2f,1f,0f,0f,true,true,false,true,2f,1f,2f,0f,true,0f,false,0f,0f,false,0f,0f,1f,1f,0f);
+    Patient diverticulosis = new Patient("Diverticulosis",Patient.Gender.FEMALE,Patient.AgeRange.ADULT,2f,0f,2f,0f,false,false,true,true,2f,0f,0f,0f,false,2f,false,1f,0f,false,0f,1f,1f,0f,0f);
+    Patient hernia = new Patient("Inguinal or Abdominal Hernia",Patient.Gender.MALE,Patient.AgeRange.CHILD,2f,0f,2f,2f,false,false,false,false,2f,0f,0f,0f,true,0f,true,2f,0f,false,0f,0f,0f,0f,0f);
+    Patient appendicitis = new Patient("Appendicitis",Patient.Gender.MALE,Patient.AgeRange.YOUNGADULT,2f,2f,2f,2f,false,false,true,true,0f,0f,0f,2f,false,0f,false,2f,0f,false,0f,0f,0f,0f,0f);
+    Patient enterocolitis = new Patient("Infectious Enterocolitis",null,Patient.AgeRange.CHILD,2f,0f,1f,1f,false,false,false,true,2f,1f,0f,0f,false,0f,false,1f,2f,true,0f,0f,0f,0f,0f);
+    Patient celiac_Disease = new Patient("Celiac Disease",Patient.Gender.FEMALE,Patient.AgeRange.ADULT,1f,2f,1f,1f,true,true,false,true,0f,1f,0f,0f,true,0f,false,0f,0f,false,0f,0f,0f,0f,0f);
+    Patient colorectal_cancer = new Patient("Colorectal Cancer",Patient.Gender.MALE,Patient.AgeRange.ADULT,2f,1f,2f,2f,true,true,true,false,2f,2f,2f,0f,true,0f,false,0f,0f,false,0f,0f,0f,0f,0f);
+         
+    
 
     private QuestionPanel current_panel;
 
@@ -136,9 +147,6 @@ public class MainWindow implements Initializable {
         enterocolitisPorcentage=patient.ComparationArrayListSymptoms(enterocolitis.getDiseaseArrayList());
         celiacPorcentage=patient.ComparationArrayListSymptoms(celiac_Disease.getDiseaseArrayList());
         colorectalCancerPorcentage=patient.ComparationArrayListSymptoms(colorectal_cancer.getDiseaseArrayList());
-        
-        
-        
         
     }
 }
